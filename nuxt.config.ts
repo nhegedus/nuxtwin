2407a225f62b7ceb92f92b546c5a3eec47a72d2d
Norbert <norbert@softgenius.com>
2023-04-18 11:40:29 +0300

    Change gh-pages deployment hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/nuxtwin/'
+    },
     css: ["@/assets/styles/styles.css"],
     build: {
         postcss: {
